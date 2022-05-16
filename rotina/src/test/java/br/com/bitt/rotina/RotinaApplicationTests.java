9ab09bb1f43a552ed2e1e6f91a8f6919c7f36b5b
MBittencourt <home@bdfti01.brisa.org.br>
2022-05-16 13:29:37 -0300

    Projeto iniciado vazio

---
+++ rotina/src/test/java/br/com/bitt/rotina/RotinaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bitt.rotina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
